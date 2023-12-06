55966446d6788911f50aea163a2568fc706c0c11
Jedidiah Thompson <wej22007@outlook.com>
2023-12-06 18:46:56 -0500

    buildsys: Kernel framework

---
+++ source/nexke/README.md
@@ -0,0 +1,2 @@
+# NexKe
+This folder contains the source to NexNix's kernel, the central component of the system
---
+++ source/nexke/cpu/i386/arch.cmake
@@ -0,0 +1,22 @@
+#[[
+    arch.cmake - contains build system for nexke i386
+    Copyright 2023 The NexNix Project
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+]]
+
+# Option for PAE
+nexnix_add_option(NEXNIX_I386_PAE "Specifies if PAE should be used" ON)
+
+# Set linker script
+set(NEXKE_LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/cpu/i386/link.ld")
